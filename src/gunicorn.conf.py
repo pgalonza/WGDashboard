5b039e4d3392217ac5a6af18ab95edaec729e436
Galonza Peter <p.galonza@evaron.ru>
2021-10-23 12:18:31 +0300

    fixed if IPv6

--- src/gunicorn.conf.py
+++ src/gunicorn.conf.py
@@ -1,6 +1,9 @@
 import dashboard
 
 app_host, app_port = dashboard.get_host_bind()
+if ':' in app_host:
+    app_host = f'[{app_host}]'
+
 bind = f"{app_host}:{app_port}"
 daemon = True
 pidfile = './gunicorn.pid'
